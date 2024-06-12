ea28edf623d8349cfc25414330211f943e21298b
mateusvicentin@hotmail.com <106186628+MateusM25@users.noreply.github.com>
2024-06-11 21:50:29 -0300

    m5_repeticoes.1

--- modulo1py/m5_repeticoes.py
+++ modulo1py/m5_repeticoes.py
@@ -118,4 +118,141 @@ while divida >= 0 :
     print(f"o Valor restante a ser pago é de {sub_total:5.2f} sendo quitado em ")   
 """
 # atribuidores de operações especiais 
+"""
+# Interronpendo repetições  
+s = 0 
+n = 0
+m = 0
+while True:
+    v = int(input("Digite um número a somar Ou 0 para sair: "))
+    if v == 0: 
+        break
+    s += v  
+    n = n + 1
+    m = s / n   
+print(f"a Soma dos Números digitados até a iunterrupção é {s:.1f}, a quantidade de vezes inserida é {n:.1f} e a média {m:5.3f}")
+"""
+"""#exerc 5.15    refazer 
+1 == "0.5"
+2 == 1.00
+3 == 4.00
+5 == 7.00
+9 == 8.00
+numeros_validos = {1,2,3,5,9}
+soma = 0 
+while True:    
+    if numeros_validos :
+    codigo = int(input("digite O codigo do produto : "))
+    print("Codigo Invalido ! ")        
+    quantidade = int(input("Digite a quantidade : "))
+    soma = quantidade * codigo
+    print(f"TOTAL: {soma}")  """
 
+"""
+#programa 5.1 - conntagem de cédulas 
+valor = int(input("Digite o valor a pagar: "))
+cedulas = 0
+atual = 50
+apagar = valor
+while True:
+    if atual <= apagar:
+        apagar -= atual
+        cedulas += 1
+    else:
+        print(f"{cedulas} Cédulas de R$ {atual}")
+        if apagar == 0:
+            break
+        if atual == 50:
+            atual = 20
+        elif atual == 20:
+            atual = 10 
+        elif atual == 10:
+            atual =5 
+        elif atual == 5 :
+            atual =1 
+        cedulas = 0 
+
+#exercicio 5.18 
+valor = float(input("Digite o valor a Pagar : "))
+cedulas = 0 
+atual = 100
+apagar = valor 
+while True :
+    if atual <= apagar : 
+       apagar -= atual 
+       cedulas += 1 
+    else :
+       print(f"{cedulas} Cédulas de R$ {atual}" )
+       if apagar == 0 :
+           break
+       if atual == 100:
+           atual = 50
+       elif atual == 50:
+           atual = 20
+       elif atual == 20 :
+           atual = 10
+       elif atual == 10:
+           atual = 5
+       elif atual == 5:
+           atual = 1
+       elif atual == 1:
+           atual = 0.5
+       elif atual == 0.5:
+           atual = 0.10
+       elif atual == 0.10 :
+           atual = 0.05
+       elif atual == 0.05:
+           atual == 0.02 
+       elif atual == 0.02 :
+           atual = 0          
+       cedulas = 0
+      """  
+
+# 5.4 Repetições aninhadas 
+"""
+tabuada = 1 
+while tabuada <= 10: 
+    numero = 1
+    while numero <= 10 :
+        print(f"{tabuada} x {numero} = {tabuada * numero}")
+        numero += 1
+    tabuada +=1
+  
+   
+   ###
+def eh_palindromo(numero):
+    # Converte o número para string
+    numero_str = str(numero)
+    
+    # Verifica se a string é igual à sua reversa
+    return numero_str == numero_str[::-1]
+
+# Exemplo de uso
+numero = int(input("Digite um número: "))
+if eh_palindromo(numero):
+    print(f"O número {numero} é um palíndromo.")
+else:
+    print(f"O número {numero} não é um palíndromo.")
+ """
+ 
+ # exercicio 5.22
+
+operacao = input("Digite a Operção que deseja obter a tabuada, Adição , Multiplicação , Subtração , Divisão  :")
+tabuada = 1
+
+while operacao == "Adição" :
+        tabuada <= 10
+        numero = 1
+        while numero <= 10 :
+            print(f"{tabuada} + {numero} = {tabuada + numero}") 
+        numero + 1
+        tabuada =     
+    
+if operacao == "Multiplicacao" :
+    print ("Multiplicação")
+elif operacao == "subtração" :
+    print("subtração")
+elif operacao == "sair":
+    print("saiu")
+    
+     
